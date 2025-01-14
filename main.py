396b1899bd256a6a72f30f5dce9e576bc1cd3cab
amer.talic <amer.talic@gmail.com>
2025-01-14 16:36:55 +0100

    deployment fix 1

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -15,3 +16,6 @@ async def read_root(request: Request):
 async def get_projects(request: Request):
     return templates.TemplateResponse("projects.html", {"request": request})
 
+if __name__ == "__main__":
+    # asyncio.run(open_browser())
+    uvicorn.run("main:app", port=3005, log_level="info", reload=False)
